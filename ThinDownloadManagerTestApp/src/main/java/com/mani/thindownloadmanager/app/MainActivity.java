d5578a594a86c04ab1411361440c5e86760a2845
Nick Koay <nick.koay@photobookworldwide.com>
2025-02-12 15:08:52 +0800

    Updated Versions and gradle

--- ThinDownloadManagerTestApp/src/main/java/com/mani/thindownloadmanager/app/MainActivity.java
+++ ThinDownloadManagerTestApp/src/main/java/com/mani/thindownloadmanager/app/MainActivity.java
@@ -3,7 +3,7 @@ package com.mani.thindownloadmanager.app;
 import android.app.AlertDialog;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
